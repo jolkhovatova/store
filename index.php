fa36e88e0bb0e926503b3f56815ad4ae256b21e6
Julia <jolkhovatova@gmail.com>
2020-01-14 18:27:25 +0200

    view

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
-class GetSet
-{
-    private $number = 1;
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', __DIR__ . DS);
+define('VIEW_DIR', ROOT . 'View' . DS);
+define('LIB_DIR', ROOT . 'Library' . DS);
 
-    public function __get($name)
-    {
-        // TODO: Implement __get() method.
-        //echo "You get {$name}";
-        if($name == 'number'){
-            // ...
-            return $this->number;
-        }
-    }
-    public function __set($name, $value)
-    {
-        // TODO: Implement __set() method.
-        echo "You set {$name}";
-    }
-}
-
-$obj = new GetSet();
-echo $obj->number;
-//echo $obj->number = 999;
-
-
+require VIEW_DIR . 'default_layout.phtml';
